b3acdba7ec73c0fc6e30c779900007090580337c
Jhollyfer Rodrigues <jhollyferr@gmail.com>
2022-07-11 19:10:59 -0300

    :tada: setting up project structure

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (request, response) => {
+    return response.json({ 
+        message: 'Hello World Ignite!' 
+    });
+});
+
+app.listen(3333, () => {
+    console.log('Server is running on port 3000');
+});
