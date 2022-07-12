58ffcc2c314a8becb36a1a4f5f1f977f57155953
Jhollyfer Rodrigues <jhollyferr@gmail.com>
2022-07-12 01:46:30 -0300

    :sparkles: create a account

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 const express = require('express');
-
+const { v4: uuidv4 } = require('uuid');
 const app = express();
+
+const customers = [];
 
 app.use(express.json());
 
 app.post('/account', (request, response) => {
+    
     const { name, cpf } = request.body;
 
-    
+    const id = uuidv4();
+
+    customers.push({
+        id,
+        name,
+        cpf,
+        statement: []
+    })
+
+    console.log(customers);
+
+    return response.status(201).send();
 });
 
 app.listen(3333, () => {
